9be157beda1a10f55d7481d0f32f985a93bdad70
КДБ <prykhozhdenko@gmail.com>
2026-05-29 08:33:52 +0300

    виправлення

--- build.sh
+++ build.sh
@@ -22,5 +22,5 @@ tar -xf "$ARCHIVE_NAME" -C "$EXTRACT_DIR"
 
 докс код --вхід="$EXTRACT_DIR/мавка-$VERSION" --вихід="$OUTPUT_DIR" --назва="мавка-$VERSION" --завантажити="https://github.com/mavka-ukr/files/raw/main/%D0%B2%D0%B8%D0%BF%D1%83%D1%81%D0%BA%D0%B8-%D0%BC%D0%B0%D0%B2%D0%BA%D0%B8/$VERSION/%D0%BC%D0%B0%D0%B2%D0%BA%D0%B0-$VERSION.tar.xz" --гітхаб="https://github.com/mavka-ukr/mavka"
 
-cp "favicon.png" "$OUTPUT_DIR/__докс_код_favicon.png"
-cp "favicon.svg" "$OUTPUT_DIR/__докс_код_favicon.svg"
+cp "favicon.png" "$OUTPUT_DIR/докс_код_favicon.png"
+cp "favicon.svg" "$OUTPUT_DIR/докс_код_favicon.svg"
